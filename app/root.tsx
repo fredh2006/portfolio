3b3ca5b6d78152bbb2ff805242dc571dce894e33
Fred He <fhe@bayviewglen.ca>
2025-06-02 13:51:16 -0400

    google analytics

--- app/root.tsx
+++ app/root.tsx
@@ -31,6 +31,18 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
+
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-PRW92TS3XJ"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-PRW92TS3XJ');
+            `,
+          }}
+        />
       </head>
       <body>
         {children}
